f2026094d0ee6a97bc64de1fc35042348ee2a626
Gauraang Goel <ggauraang@gmail.com>
2024-04-13 17:12:48 +0530

    Falling Audio added

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -6,13 +6,14 @@ public class Obstacles : MonoBehaviour
 {
     public GameObject Model;
     public GameObject LevelGenerate;
-    [SerializeField] private AudioSource coin;
+    [SerializeField] private AudioSource fall;
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Obstacles")){
             gameObject.GetComponent<BoxCollider>().enabled = false;
             gameObject.GetComponent<PlayerMovement>().enabled = false;
             Model.GetComponent<Animator>().SetTrigger("Death");
+            fall.Play();
             LevelGenerate.GetComponent<LevelGenerator>().enabled = false;
 
         }
